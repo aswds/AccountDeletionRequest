0682b3b165e2b21939b6acfc5c5640dc37ba51e7
Olexandr Ilnitskiy <64701637+aswds@users.noreply.github.com>
2023-08-25 01:54:10 +0300

    [refactor] - delete unused code

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,15 +5,7 @@ import {
   browserLocalPersistence,
 } from "firebase/auth";
 // TODO: Replace the following with your app's Firebase project configuration
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-// };
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
