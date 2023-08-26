0a0fccc5680f0870e8ee1eca0b6968fa44b3e899
aswds <sasha.iilllnitskiy@gmail.com>
2023-08-26 14:03:55 +0300

    .

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {
-  initializeAuth,
-  getAuth,
-  browserLocalPersistence,
-} from "firebase/auth";
+import { getAuth } from "firebase/auth";
 // TODO: Replace the following with your app's Firebase project configuration
 
 const firebaseConfig = {
